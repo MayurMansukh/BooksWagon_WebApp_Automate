104a670ef0b97bbed9b890face1a0c0939f146ad
MayurMansukh <mansukhmayur99@gmail.com>
2021-06-20 10:43:25 +0530

    add logic to test App login with multiple data using data driven framework

---
+++ src/test/java/com/bridgelabz/test/LoginWithMultipleData.java
@@ -0,0 +1,45 @@
+/**
+ * Description : add Test to BooksWagon webApp Login with multiple data
+ * Author      : Mayur Mansukh
+ * Date        : 18/6/21
+ */
+package com.bridgelabz.test;
+
+import com.bridgelabz.base.Base;
+import com.bridgelabz.pages.LoginPage;
+import com.bridgelabz.utils.DataDrivenMethod;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.testng.annotations.Test;
+
+public class LoginWithMultipleData extends Base {
+
+    @Test
+    public void LoginTest() throws Exception {
+        DataDrivenMethod drivenMethod = new DataDrivenMethod();
+        LoginPage loginPage = new LoginPage(driver);
+
+        XSSFSheet sheet = drivenMethod.excel_utils_base().getSheetAt(0);
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            System.out.println("test started with data " + i);
+            loginPage.clickLogin();
+            Thread.sleep(1000);
+            String username1 = String.valueOf(sheet.getRow(i).getCell(0));
+            String password1 = String.valueOf(sheet.getRow(i).getCell(1));
+
+            loginPage.enterUserName(username1);
+            Thread.sleep(1000);
+            loginPage.enterPassword(password1);
+            Thread.sleep(1000);
+            boolean check = loginPage.clickSubmitBtn();
+            if(check) {
+                Thread.sleep(1000);
+                loginPage.clickLogoutopt();
+                loginPage.clickLogoutBtn();
+                System.out.println("test success with data " + i);
+            }
+            else
+                System.out.println("test failed with data " + i);
+        }
+
+    }
+}
