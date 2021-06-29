ae9c15936d2d48285dc6c155ce8cc3f525d577f9
MayurMansukh <manojmansukh7004>
2021-06-29 19:15:27 +0530

    miodify the code to create testNg file into project

--- src/main/java/com/bridgelabz/utils/ExcelReport.java
+++ src/main/java/com/bridgelabz/utils/ExcelReport.java
@@ -2,6 +2,7 @@
  * Description : add Excel report method
  * Author      : Mayur Mansukh
  * Date        : 9/6/21
+<<<<<<< HEAD
  */
 //package com.bridgelabz.utils;
 //
@@ -11,3 +12,16 @@
 //    public void ExcelReport() throws Exception {
 //        Xl.generateReport("excel-report.xlsx");
 //    }
+=======
+ 
+package com.bridgelabz.utils;
+
+import org.automationtesting.excelreport.Xl;
+
+public class ExcelReport {
+    public void ExcelReport() throws Exception {
+        Xl.generateReport("excel-report.xlsx");
+    }
+}
+*/
+>>>>>>> 52dc355c9f5742916e55ed0899c420f2b81c1865
